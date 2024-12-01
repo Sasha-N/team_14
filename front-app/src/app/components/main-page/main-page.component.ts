df320510f3eea113ad704f40e87586c8bfb62f85
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2024-12-01 20:54:17 +0700

    navtool for main page

--- front-app/src/app/components/main-page/main-page.component.ts
+++ front-app/src/app/components/main-page/main-page.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
