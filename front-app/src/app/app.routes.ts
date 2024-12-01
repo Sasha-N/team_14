2ac79a2113654cc004f2be10700fcba81ca32e91
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2024-12-01 17:53:53 +0700

    fix routes

--- front-app/src/app/app.routes.ts
+++ front-app/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 export const routes: Routes = [{
     path: 'login',
     component: LoginPageComponent
+  },
+  {
+    path: '',
+    component: MainPageComponent
   }];
